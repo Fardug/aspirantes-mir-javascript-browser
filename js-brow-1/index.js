d80e782e58700c6dd7c6c30919bb4b6964e2adcb
Fidel <fidel9520@gmail.com>
2023-03-18 11:57:31 -0500

    Cambios 5

--- js-brow-1/index.js
+++ js-brow-1/index.js
@@ -4,16 +4,16 @@ let btn = document.getElementsByClassName("btn")[0];
 let resultadomayusculas = document.getElementById("resultadoMayusculas");
 console.log("resultado mayuscla", resultadomayusculas);
 
- input.addEventListener('input',function(){
+input.addEventListener('input', function () {
     let mayus = getTexto();
     resultado.textContent = mayus;
 });
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function () {
     let mayus = getTexto();
     resultadomayusculas.textContent = mayus;
 });
 
-let getTexto = function(){
-return input.value.toUpperCase();
+let getTexto = function () {
+    return input.value.toUpperCase();
 };
